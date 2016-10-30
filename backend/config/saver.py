f3b2e3abd6b6c2e73362026251b11d9e9e2dcd19
Samuel Vargas <savarga1@asu.edu>
2016-10-30 12:28:40 -0700

    removed extraneous backend modules

--- backend/config/saver.py
+++
@@ -1,7 +0,0 @@
-"""
-    saver.py
-    Author: Samuel Vargas
-
-    This module is responsible for saving the current configuration file in
-    memory to disk.
-"""
--- backend/player/player.py
+++
@@ -1,6 +0,0 @@
-"""
-player.py
-Author: Samuel Vargas
-
-This module represents the actual audio player to be used in the program server side.
-"""
