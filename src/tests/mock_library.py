2226eaa678d644145a403f88ffc6a8bfc4f5e85c
Samuel Vargas <savarga1@asu.edu>
2016-11-05 14:30:13 -0700

    create stub taglist test and mock library modules

---
+++ src/tests/mock_library.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#
+#   mock_library.py
+#   Author: Samuel Vargas
+#   Date: 10/29/2016
--- src/tests/test_taglist.py
+++ src/tests/test_taglist.py
@@ -3,5 +3,11 @@
 #   Author: Samuel Vargas
 #   Date: 11/04/2016
 #
-#   TODO: Implement
-#
+#   This test ensures that the taglist module is functioning properly
+
+from unittest import TestCase
+
+import tempfile
+
+class TagListTest(TestCase):
+    pass
