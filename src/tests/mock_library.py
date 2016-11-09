de7d0d4564a2bfd6b598b1feb8575ae28456a87c
Samulus <Samulus@users.noreply.github.com>
2016-11-09 13:57:54 -0700

    add mock_library.py module and searching tests for test_taglist.py

--- src/tests/mock_library.py
+++ src/tests/mock_library.py
@@ -2,4 +2,35 @@
 #
 #   mock_library.py
 #   Author: Samuel Vargas
-#   Date: 10/29/2016
+#   Date: 11/08/2016
+
+import shutil
+import taglib
+from tempfile import mkdtemp, NamedTemporaryFile
+from typing import List, Dict
+
+
+class MockLibrary:
+    def __init__(self, mock_files: List[Dict[str, str]]):
+        self.mock_files = mock_files
+        self.path = mkdtemp() or None
+
+    def __enter__(self):
+        for entry in self.mock_files:
+            # create dummy mp3 file for taglib
+            tmp_file = NamedTemporaryFile(suffix=".mp3", delete=False, dir=self.path)
+            tmp_file.write(b'ID3')
+            tmp_file.close()
+
+            # open them up in taglib and write the requested tags to each file
+            dummy_audio = taglib.File(tmp_file.name)
+            for tag_key, tag_value in entry.items():
+                dummy_audio.tags[tag_key.upper()] = tag_value
+            dummy_audio.save()
+            dummy_audio.close()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.path is not None:
+            shutil.rmtree(self.path)
+
--- src/tests/test_taglist.py
+++ src/tests/test_taglist.py
@@ -1,13 +1,69 @@
 #
 #   test_taglist.py
 #   Author: Samuel Vargas
-#   Date: 11/04/2016
+#   Date: 11/09/2016
 #
-#   This test ensures that the taglist module is functioning properly
+#   This test ensures that the taglist module is functioning properly,
+#   we need to add more tests to handle erroneous data and other situations
+#   but this module is a start.
 
 from unittest import TestCase
+from src.tests.mock_library import MockLibrary
+from src.taglist import TagList
 
-import tempfile
 
 class TagListTest(TestCase):
-    pass
+    TEST_DATA = [
+        # Death Grips
+        {"artist": "Death Grips", "album": "The Money Store", "title": "Get Got"},
+        {"artist": "Death Grips", "album": "The Money Store", "title": "The Fever (Aye Aye)"},
+        {"artist": "Death Grips", "album": "The Money Store", "title": "Lost Boys"},
+        {"artist": "Death Grips", "album": "Government Plates", "title": "Pillbox Hat"},
+        {"artist": "Death Grips", "album": "Government Plates", "title": "Anne Bonny"},
+        {"artist": "Death Grips", "album": "Government Plates", "title": "Two Heavens"},
+        # Jeremiah Jae
+        {"artist": "Jeremiah Jae", "album": "Raw Money Raps", "title": "Seasons"},
+        {"artist": "Jeremiah Jae", "album": "Raw Money Raps", "title": "Cable"},
+        {"artist": "Jeremiah Jae", "album": "Raw Money Raps", "title": "Cat Fight"},
+    ]
+
+    @staticmethod
+    def test_search_for_single_album():
+        """
+        Test searching for a single album in the library. This test looks for the album
+        'The Money Store' and should only retrieve results that match.
+        """
+
+        # record the number of entries in TEST_DATA with album tag 'The Money Store'
+        money_store_entries = len(
+            [x for x in TagListTest.TEST_DATA if 'album' in x and x['album'] == 'The Money Store']
+        )
+
+        # generate a list of the expected titles
+        money_store_titles = tuple([x['title'] for x in TagListTest.TEST_DATA if
+                              'title' in x and 'album' in x and x['album'] == 'The Money Store'])
+
+        with MockLibrary(TagListTest.TEST_DATA) as library:
+            taglist = TagList(audio_directory=library.path)
+            search_results = taglist.search(album="Money Store")
+            # ensure we find every album that is tagged with 'The Money Store'
+            assert (len(search_results) == money_store_entries)
+            # ensure that the titles are correct in the search results
+            for result in search_results:
+                assert result["title"][0].endswith(money_store_titles)
+
+    @staticmethod
+    def test_search_for_multiple_album():
+        """
+        If the user searches for 'Money' it should return all albums that contain the (case insensitive string)
+        'Money' in the title. In the case of the test data it should return all songs from 'The Money Store' as well
+        as 'Raw Money Raps'.
+        """
+        albums_with_money_in_title = len([x for x in TagListTest.TEST_DATA if 'album' in x and 'Money' in x['album']])
+        shared_album_titles = tuple([x['title'] for x in TagListTest.TEST_DATA if 'album' in x and 'Money' in x['album']])
+        with MockLibrary(TagListTest.TEST_DATA) as library:
+            taglist = TagList(audio_directory=library.path)
+            search_results = taglist.search(album="Money")
+            assert(len(search_results) == albums_with_money_in_title)
+            for result in search_results:
+                assert result["title"][0].endswith(shared_album_titles)
