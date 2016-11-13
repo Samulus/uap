8d887adf054a757498a1410b432887674e515367
Samuel Vargas <savarga1@asu.edu>
2016-11-13 15:13:49 -0700

    added stub sessiondb and userdb modules, these should be implemented

---
+++ src/sessiondb.py
@@ -0,0 +1,13 @@
+#
+#   sessiondb.py
+#   Author: Samuel Vargas
+#   Date: 11/12/2016
+#
+#   Maintains a list of users and what they have access to / privileges
+
+class SessionDB:
+    def __init__(self):
+        pass
+
+    def STUB_user_is_logged_in(self, return_truth=True) -> bool:
+        return return_truth
