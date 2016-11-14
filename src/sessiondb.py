52ddf67bd3fbd06e29e1ae94f8ac7d03fcb79fbb
Samulus <Samulus@users.noreply.github.com>
2016-11-14 16:41:48 -0700

    added sessiondb stubs

--- src/sessiondb.py
+++ src/sessiondb.py
@@ -3,11 +3,19 @@
 #   Author: Samuel Vargas
 #   Date: 11/12/2016
 #
-#   Maintains a list of users and what they have access to / privileges
+#   Maintains a list of all the currently logged in users
+#   and allows the server to verify if a user is actually
+#   logged in or not.
+
+from src.userdb import UserDB
+
 
 class SessionDB:
     def __init__(self):
         pass
 
-    def STUB_user_is_logged_in(self, return_truth=True) -> bool:
-        return return_truth
+    def user_is_logged_in(self):
+        raise NotImplementedError
+
+    def token_is_valid(self):
+        raise NotImplementedError
