c0056e79faa0dc2a2cdfc983585bfe03527d29c4
Samuel Vargas <savarga1@asu.edu>
2016-11-16 11:58:44 -0700

    add stub test_user db module
    This module is pretty important and
    absolutely should be implemented eventually.

---
+++ src/tests/test_userdb.py
@@ -0,0 +1,34 @@
+#
+#   test_userdb.py
+#   Author: Samuel Vargas
+#   Date: 11/09/2016
+#
+#   This module is responsible for testing the userdb.py module
+#   for bugs, security issues, and ease of use. It should ensure that
+#   the userdb module is free from injection attacks / other security
+#   problems etc.
+
+import unittest
+
+
+class UserDBTest(unittest.TestCase):
+    def test_add_user(self):
+        raise NotImplementedError
+
+    def test_avoid_adding_duplicate_user(self):
+        raise NotImplementedError
+
+    def test_get_user_info(self):
+        raise NotImplementedError
+
+    def test_is_correct_password(self):
+        raise NotImplementedError
+
+    def test_remove_user(self):
+        raise NotImplementedError
+
+    def test_injection_attacks(self):
+        raise NotImplementedError
+
+    def test_change_password(self):
+        raise NotImplementedError
