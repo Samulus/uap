24f8b87442a82365aede43c860f3eb8a5ba97fd0
Samuel Vargas <sam@windows>
2016-11-18 23:49:02 -0700

    tweak starting music path, change this to whatever on your system

--- src/uap.py
+++ src/uap.py
@@ -6,19 +6,19 @@
 #   TODO: introduce a configuration file so that users don't
 #   have to manually specify the path of their music library
 #   everytime they want to run the server.
-   
 
-from src.taglist import TagList
+
 from src.server import Server
+from src.taglist import TagList
 from src.userdb import UserDB
 
 if __name__ == '__main__':
     server = Server(
         host='127.0.0.1',
-        taglist=TagList("/home/sam/music/"),
+        taglist=TagList("D:\music"), # change this to your music folder
         userdb=UserDB(),
         debug=True,
         reloader=False,
-        login_required=False,
+        login_required=True,
     )
     server.start()
