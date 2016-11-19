dd4cd1f55fd5600eabf6230b560a07b6dc23b9a3
Samuel Vargas <sam@windows>
2016-11-18 23:51:40 -0700

    switch from mock_library module to actually mocking the method out

--- src/mocks/mock_server.py
+++ src/mocks/mock_server.py
@@ -15,21 +15,21 @@
 #   In other words this module is fully portable provided you don't
 #   move data/moonlight_jazz.mp3 or data/tags.json
 
+from os.path import join, realpath
 from unittest import mock
 
-from src.taglist import TagList
-from src.userdb import UserDB
-from src.server import Server
-from src.mocks.mock_library import MockLibrary
 from bottle import static_file, response
-from os.path import join, realpath
-import json
+
+from src.server import Server
+from src.userdb import UserDB
 
 ROOT_PATH = realpath(join(__file__, ".."))
 MOCK_TAGS_PATH = join(ROOT_PATH, "data/tags.json")
+MOCK_LIBRARY_HIERARCHY = join(ROOT_PATH, "data/library_hierarchy.json")
 
 
-def get_flamenco(self, song_path: str):
+# server.py: Stubbed out methods
+def get_song(self, song_path: str):
     if self.session_is_valid():
         response.status = 200
         return static_file("moonlight_jazz.mp3",
@@ -38,28 +38,23 @@ def get_flamenco(self, song_path: str):
         response.status = 403
 
 
+def get_library(self):
+    if self.session_is_valid():
+        response.status = 200
+        with open(MOCK_LIBRARY_HIERARCHY, "r") as library_hierarchy:
+            return library_hierarchy.read()
+    else:
+        response.status = 403
+
+
 if __name__ == '__main__':
-    tags_to_write = None
-    with open(MOCK_TAGS_PATH, "r") as file:
-        tags_to_write = json.loads(file.read())
-
-    # create the mock library
-    mock_library = MockLibrary(mock_files=tags_to_write)
-
-    # create a taglist using data from mock_library
-    mock_taglist = TagList(mock_library.path, ram_db=True)
-
-    # delete the temporary files, we don't need them
-    # since we're monkey patching the audio code anyway
-    mock_library.cleanup()
-
-    # create the server
-    with mock.patch('src.server.Server.get_song', get_flamenco):
+    with mock.patch('src.server.Server.get_song', get_song), \
+         mock.patch('src.server.Server.get_library', get_library):
         server = Server(
-            taglist=mock_taglist,
+            taglist=None,
             userdb=UserDB(ram_db=True),
             debug=True,
             login_required=False,
-            reloader=True
+            reloader=False
         )
         server.start()
