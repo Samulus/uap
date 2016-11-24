573a80dd2c3c648e4c75ca465b2a2b24d4a46c4a
Samuel Vargas <savarga1@asu.edu>
2016-11-24 15:34:36 -0700

    minor linting to login.js and route.js

--- src/client/js/login.js
+++ src/client/js/login.js
@@ -1,8 +1,8 @@
 /*
-    login.js
-    Author: Samuel Vargas
-    Date: 11/14/2016
-*/
+ login.js
+ Author: Samuel Vargas
+ Date: 11/14/2016
+ */
 
 Vue.http.options.emulateJSON = true;
 var login_vm = new Vue({
@@ -12,23 +12,23 @@ var login_vm = new Vue({
         got_message_from_server: false,
         message: '',
         username: '',
-        password: '',
+        password: ''
     },
 
     methods: {
-        action: function(action_type, event) {
+        action: function (action_type, event) {
             event.preventDefault();
             Vue.http.post('api/' + action_type, {
                 'username': this.$data.username,
                 'password': this.$data.password,
             }).then(
                 function success(response) {
-                    location.reload()
+                    location.reload();
                 },
                 function failure(response) {
                     login_vm.$data.got_message_from_server = true;
                     login_vm.$data.message = response.statusText;
-                    setTimeout(function() {
+                    setTimeout(function () {
                         login_vm.$data.got_message_from_server = false;
                     }, 3000);
                 }
