e84ec864de6cd17b9bd41778ba2afcba4dc7a420
Sean McGonegle <smcgonegle@gmail.com>
2016-11-28 12:06:47 -0700

    Better Test
    Dunno what I was doing with the other one

--- src/tests/test_config.py
+++ src/tests/test_config.py
@@ -1,38 +1,20 @@
 import unittest
 from unittest import TestCase
 
+import src.config
+from configparser import ConfigParser
 from os.path import realpath, join, exists
 
-import src.config
+CONFIG_PATH = realpath(join(__file__, "..", "config.ini"))
 
-DEFAULT_CONFIG = {
-    'host': '127.0.0.1',
-    'port': 8080,
-    'debug': False,
-    'reloader': False,
-    'login_required': False,
-    'music_folder': None,
-}
+EXPECTED_INI ={'port': 8080, 'host': '127.0.0.1', 'login_required': True, 'reloader': False, 'debug': False, 'music_folder': None}
 
 class UserDBTest(TestCase):
     def test_load_empty_path(self):
-        self.assertIsNone(src.config.load_settings_dict())
+        self.assertIsNotNone(src.config.load_settings_dict())
 
-    def test_cant_find_settings(self):
-        EXAMPLE_INI = """
-        host = 127.0.0.1
-        port = 8080
-        debug = False
-        reloader = False
-        login_required = True
-        #music_folder = uncomment this, put your music folder here
-        """
-        CONFIG_PATH = realpath(join(__file__, "..", "config.ini"))
-        src.config.load_settings_dict()
-        with self.assertRaises(SyntaxError) :
-            print("Missing [settings] at top of config.ini")
-
-
+    def test_file(self):
+        self.assertEqual(src.config.load_settings_dict(),EXPECTED_INI)
 
 if __name__ == '__main__':
     unittest.main()
